04d4b2e03e195fd94994393ed86587368da4c273
Jaracah Teague <jaracahteague@jkt-mb.lan>
2022-03-05 23:31:32 -0500

    sel

--- selenium/apex.py
+++ selenium/apex.py
@@ -17,7 +17,7 @@ import os
 
 # options
 overwrite = True
-headless = True
+headless = False
 
 # urls
 home = 'https://apex.tracker.gg/'
@@ -39,82 +39,85 @@ else:
 
 # -----------------------------------------------------------------------------------------------------
 # get leaderboard data
+try:
+	# go to the leaderboard page
+	driver.get(leaderboard)
+	time.sleep(1)
 
-# go to the leaderboard page
-driver.get(leaderboard)
-time.sleep(1)
+	# get all hrefs in table and loop
+	links = driver.find_elements_by_css_selector('tr > td.username > div.text > a')
+	link_hrefs = [link.get_attribute('href') for link in links]
 
-# get all hrefs in table and loop
-links = driver.find_elements_by_css_selector('tr > td.username > div.text > a')
-link_hrefs = [link.get_attribute('href') for link in links]
+	# looping through each page
+	for link in link_hrefs:
 
-# looping through each page
-for link in link_hrefs:
+		# filter bad links
+		if "twitch" in link:
+			continue
+		elif "twitter" in link:
+			continue
 
-	# filter bad links
-	if "twitch" in link:
-		continue
-	elif "twitter" in link:
-		continue
+		# go to link and wait --possibly use wait for element functions
+		driver.get(link)
+		time.sleep(1) 
 
-	# go to link and wait --possibly use wait for element functions
-	driver.get(link)
-	time.sleep(1) 
+		# ==================================================================================================
+		# getting data values from the page
 
-	# ==================================================================================================
-	# getting data values from the page
+		# player id
+		user_id = driver.find_element_by_css_selector('span.trn-ign__username').text
 
-	# player id
-	user_id = driver.find_element_by_css_selector('span.trn-ign__username').text
+		# views 
+		views = driver.find_element_by_css_selector('div.ph-details__subtitle > span > span').text
 
-	# views 
-	views = driver.find_element_by_css_selector('div.ph-details__subtitle > span > span').text
+		# stats --neds to be fixed
+		stats 	= driver.find_elements_by_css_selector('div.wrapper > div.numbers > span.value')
+		level 	= stats[0].text
+		kills 	= stats[1].text
+		# damage 	= stats[2].text
+		# matches = stats[3].text
 
-	# stats --neds to be fixed
-	stats 	= driver.find_elements_by_css_selector('div.wrapper > div.numbers > span.value')
-	level 	= stats[0].text
-	kills 	= stats[1].text
-	# damage 	= stats[2].text
-	# matches = stats[3].text
+		# ranked and arenas stats
+		rank 	= driver.find_elements_by_css_selector('div.rating-entry__rank > div > div.rating-entry__rank-info > div.label')
+		mmr 	= driver.find_elements_by_css_selector('div.rating-entry__rank > div > div.rating-entry__rank-info > div.value > span')
 
-	# ranked and arenas stats
-	rank 	= driver.find_elements_by_css_selector('div.rating-entry__rank > div > div.rating-entry__rank-info > div.label')
-	mmr 	= driver.find_elements_by_css_selector('div.rating-entry__rank > div > div.rating-entry__rank-info > div.value > span')
+		br_rank = rank[0].text.strip()
+		br_mmr 	=  mmr[0].text.strip()
 
-	br_rank = rank[0].text.strip()
-	br_mmr 	=  mmr[0].text.strip()
+		ar_rank = rank[1].text.strip()
+		ar_mmr 	=  mmr[1].text.strip()
 
-	ar_rank = rank[1].text.strip()
-	ar_mmr 	=  mmr[1].text.strip()
+		# gets most recently used legend
+		legend = driver.find_element_by_css_selector('div.legend__name').text
 
-	# gets most recently used legend
-	legend = driver.find_element_by_css_selector('div.legend__name').text
-
-	# ==================================================================================================
+		# ==================================================================================================
 
 
-	# create data and append to json lines file
-	data = {
-	
-		'user_id'	: user_id,
-		'views'		: views,
-		'level'		: level,
-		'kills'		: kills,
-		# 'damage'	: damage,
-		# 'matches'	: matches,
-		'br_rank'	: br_rank,
-		'br_mmr'	: br_mmr,
-		'ar_rank'	: ar_rank,
-		'ar_mmr'	: ar_mmr,
-		'legend'	: legend,
+		# create data and append to json lines file
+		data = {
+		
+			'user_id'	: user_id,
+			'views'		: views,
+			'level'		: level,
+			'kills'		: kills,
+			# 'damage'	: damage,
+			# 'matches'	: matches,
+			'br_rank'	: br_rank,
+			'br_mmr'	: br_mmr,
+			'ar_rank'	: ar_rank,
+			'ar_mmr'	: ar_mmr,
+			'legend'	: legend,
 
-	}
+		}
 
-	with open('apex.jl', 'a') as fp:
-		fp.write(json.dumps(data))
-		fp.write('\n')
+		with open('apex.jl', 'a') as fp:
+			fp.write(json.dumps(data))
+			fp.write('\n')
 
 # -----------------------------------------------------------------------------------------------------
-
 # close webdriver
-driver.close()
+except:
+	driver.quit()
+
+finally:
+	driver.quit()
